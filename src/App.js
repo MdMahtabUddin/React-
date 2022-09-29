c297501f9aaf104a35a36aee9ece4ecf352b1a3b
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-09-29 09:47:00 +0000

    edit done

--- src/App.js
+++ src/App.js
@@ -2,26 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const nayoks = ['Rubel', 'Bapparaz', 'Koober', 'Bappi', 'Shuvo', 'Omar Sani'];
-
-  const cinemas = [
-    { nayok: 'Koober', nayika: 'kopila' },
-    { nayok: 'Rubel', nayika: 'Moushumi' },
-    { nayok: 'Razzak', nayika: 'Shabana' },
-    { nayok: 'Jashim', nayika: 'Suchorita' },
-
-  ]
+  
   return (
     <div className="App">
-      <ul>
-        {
-          nayoks.map(nayok => <li>{nayok}</li>)
-        }
-      </ul>
-      {/* <Cinema nayok="J Bond" nayika="Cat W"></Cinema> */}
-      {
-        cinemas.map(cinema => <Cinema nayok={cinema.nayok} nayika={cinema.nayika}></Cinema>)
-      }
+      
 
     </div>
   );
