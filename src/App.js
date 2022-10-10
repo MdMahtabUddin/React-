dbcb8cbf27b798070b3f941dbadb1ce8d6ce47a0
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-10-10 20:47:51 +0000

    create component

--- src/App.js
+++ src/App.js
@@ -6,11 +6,24 @@ function App() {
   return (
     <div className="App">
       
+      <Person></Person>
 
     </div>
   );
 }
 
+// কম্পোনেন্ট বানানো , অনেক টা html ট্যাগের মতই 
+// 45-3 Create Component, return HTML from a component
+
+function Person(){
+  return (
+    <div>
+    <h1>Shakib al Hasan</h1>
+    <p>Profession: cricketer</p>
+
+  </div>
+  )
+};
 
 
 
